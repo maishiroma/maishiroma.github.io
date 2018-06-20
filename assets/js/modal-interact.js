602dd892d6640a7b201faf3970b47294103d8087
shiro105 <shiro105@mail.chapman.edu>
2018-06-20 14:47:37 -0700

    Started filling in details for each portfolio piece

--- assets/js/modal-interact.js
+++ assets/js/modal-interact.js
@@ -21,6 +21,9 @@ var modalVideoImage = document.getElementById("modalVideoImage");
 var modalSlides = document.getElementsByClassName("slides");
 var modalDots = document.getElementsByClassName("dot");
 var modalReturn = document.getElementById("returnIcon");
+var modalImage1Link = document.getElementById("ModalImage1Expand");
+var modalImage2Link = document.getElementById("ModalImage2Expand");
+var modalImage3Link = document.getElementById("ModalImage3Expand");
 
 // Specific elements in the All Modal
 var allModal = document.getElementById("AllModal");
@@ -121,64 +124,126 @@ function expandPreview(idOfPreview)
 	switch(idOfPreview)
 	{
           case "Android":
-               message = "Under Construction";
                header = "Character Picker App";
+               message =
+               "<p>An Android application that demonstrates all concepts learned in the Android App Development class.</p>" +
+               "<ol>" +
+               "<li>Included Fragment and Activity relationship, passing data between them, RecycleViews, user interaction (swipe, press, drag and drop)</li>" +
+               "<li>Went beyond the class scope by implementing a local database using SQLite and image management</li>" +
+               "</ol>" +
+               "<p>Functions include adding new items, editing an item, moving an item, deleting an item and rating an item.<p>" +
+               "<ol>" +
+               "<li>Each item has their own image associated with it as well as an average rating</li>" +
+               "<li>Can swipe between items when viewing an item</li>";
                modalImage1.src = "assets/images/portfolio/Android1.png";
                modalImage2.src = "assets/images/portfolio/Android2.png";
-               modalVideoImage.src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif";
+               modalVideoImage.src = "assets/images/portfolio/Android3.png";
                hasVideo = false;
                break;
           case "Chat":
-               message = "Under Construction";
                header = "Online Chat Application";
+               message =
+               "<p>The final project for the Programming Languages course that showcased the best use case for the language each student studied over the semester.</p>" +
+               "<ol>" +
+               "<li>Created a web browser chat room that allowed 10 people to chat remotely</li>" +
+               "<ul>" +
+               "<li>Backend done in Elixir with the front end done in JavaScript</li>" +
+               "</ul>" +
+               "<li>Features included nicknames, monitored who is online and automated messages whenever users enter and leave the room</li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/Chat3.png";
                modalImage2.src = "assets/images/portfolio/Chat1.png";
                modalVideoImage.src = "assets/images/portfolio/Chat2.png";
                hasVideo = false;
                break;
           case "Drumpf":
-               message = "Under Construction";
                header = "Sentiment Twitter Analysis Program";
+               message =
+               "<p>The final project of the Data Communications and Networks class.</p>" +
+               "<li>A Python program that took a random top trend on Twitter and retweeted a related tweet that has the highest sentiment value every 30 minutes</li>" +
+               "<li>Took advantage of an open sourced Twitter API</li>" +
+               "<li>Submitted to the December 2016 Student Research Day</li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/Drumpf3.png";
                modalImage2.src = "assets/images/portfolio/Drumpf1.png";
                modalVideoImage.src = "assets/images/portfolio/Drumpf2.png";
                hasVideo = false;
                break;
           case "ISD":
-               message = "Under Construction";
                header = "In Solo Duo";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/ISD1.png";
                modalImage2.src = "assets/images/portfolio/ISD2.png";
                modalVideoImage.src = "assets/images/portfolio/ISD3.png";
                hasVideo = false;
                break;
           case "QMerit":
-               message = "Under Construction";
                header = "QMerit Internship";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/Qmerit1.jpg";
                modalImage2.src = "assets/images/portfolio/Qmerit2.jpg";
                modalVideoImage.src = "assets/images/portfolio/Qmerit3.png";
                hasVideo = false;
                break;
           case "Reflections":
-               message = "Under Construction";
                header = "Reflections";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/Reflections3.png";
                modalImage2.src = "assets/images/portfolio/Reflections2.png";
                modalVideoImage.src = "assets/images/portfolio/Reflections1.png";
                hasVideo = false;
                break;
           case "TSG":
-               message = "Under Construction";
                header = "Top Shelf Gaming Internship";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/TSG1.png";
                modalImage2.src = "assets/images/portfolio/TSG2.png";
                modalVideoImage.src = "assets/images/portfolio/TSG4.jpeg";
                hasVideo = false;
                break;
 		case "ROC":
-			message = "Under Construction";
 			header = "Arvada Labs Internship";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
 			modalImage1.src = "assets/images/portfolio/ROC1.png";
 			modalImage2.src = "assets/images/portfolio/ROC2.png";
                modalVideoImage.src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif";
@@ -186,8 +251,16 @@ function expandPreview(idOfPreview)
                hasVideo = true;
 			break;
 		case "Astrae":
-			message = "Under Construction";
 			header = "Astrae";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
 			modalImage1.src = "assets/images/portfolio/Astrae1.png";
 			modalImage2.src = "assets/images/portfolio/Astrae2.png";
                modalVideoImage.src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif";
@@ -195,8 +268,16 @@ function expandPreview(idOfPreview)
                hasVideo = true;
 			break;
           case "BSEC":
-               message = "Under Construction";
                header = "Beyond Space Energy Corp";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/BSEC1.png";
                modalImage2.src = "assets/images/portfolio/BSEC3.png";
                modalVideoImage.src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif";
@@ -204,32 +285,56 @@ function expandPreview(idOfPreview)
                hasVideo = true;
                break;
           case "Catapult":
-               message = "Under Construction";
                header = "Steam Punk Catapult Modal";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/modals/Catapult2.jpg";
                modalImage2.src = "assets/images/portfolio/modals/Catapult3.jpg";
                modalVideoImage.src = "assets/images/portfolio/modals/Catapult4.jpg";
                hasVideo = false;
                break;
           case "LowPoly":
-               message = "Under Construction";
                header = "Low Poly Art for BSEC";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/modals/Flowers.jpg";
                modalImage2.src = "assets/images/portfolio/modals/Mushroom.jpg";
                modalVideoImage.src = "assets/images/portfolio/modals/Tree.jpg";
                hasVideo = false;
                break;
           case "Odyssey":
-               message = "Under Construction";
                header = "The Odyssey from Super Mario Odyseey";
+               message =
+               "<p></p>" +
+               "<ol>" +
+               "<li></li>" +
+               "<ul>" +
+               "<li></li>" +
+               "</ul>" +
+               "<li></li>" +
+               "</ol>"
                modalImage1.src = "assets/images/portfolio/modals/Odyssey2.jpg";
                modalImage2.src = "assets/images/portfolio/modals/Odyssey3.jpg";
                modalVideoImage.src = "assets/images/portfolio/modals/Odyssey4.jpg";
                hasVideo = false;
                break;
 		default:
-			message = "OH NO";
 			header = "Woah!";
+               message = "<p class='modal-body-text'>OH NO</p>";
                modalImage1.src = "assets/images/Custom404.png";
                modalImage2.src = "assets/images/Custom404.png";
                modalVideoImage.src = "assets/images/404GIF.gif";
@@ -241,7 +346,18 @@ function expandPreview(idOfPreview)
 	modal.style.display = "block";
 	modalParagraph.innerHTML = message;
 	modalHeader.innerHTML = header;
-
+     // We associate the image links with their respective owners so when the user clicks them, they are expanded
+     modalImage1Link.href = modalImage1.src;
+     modalImage2Link.href = modalImage2.src;
+     // If there is a video with the slides, the third image does nothing when clicked
+     if(hasVideo == true)
+     {
+          modalImage3Link.href = "javascript: void(0)";
+     }
+     else
+     {
+          modalImage3Link.href = modalVideoImage.src;
+     }
 	showSlides(slideIndex);
 }
 
@@ -419,6 +535,6 @@ function filterSelection(element, criteria)
 function bringUpNewPreview(nameOfItem)
 {
      allModal.style.display = "none";
-     expandPreview(nameOfItem);
      modalReturn.style.display = "inherit";
+     expandPreview(nameOfItem);
 }
